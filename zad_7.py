53dbe6146f0b47bcef7ea6f8035aa7e4889a86c1
AnnaCichon <anna.cichon97@wp.pl>
2021-11-03 14:41:42 +0100

    dodanie nowego pliku

--- zad_7.py
+++ zad_7.py
@@ -5,7 +5,13 @@ response = r.json()
 
 
 class Brawery:
-    def __init__(self, id: int, name: str, brewery_type: str, street: str, address_2: str, address_3: str, city: str, state: str, county_province: str, postal_code: str, country: str, longitude: str, latitude: str, phone: str, website_url: str, updated_at: str, created_at: str):
+
+    def __init__(self, id: int, name: str, brewery_type: str,
+                 street: str, address_2: str, address_3: str,
+                 city: str, state: str, county_province: str,
+                 postal_code: str, country: str, longitude: str,
+                 latitude: str, phone: str, website_url: str,
+                 updated_at: str, created_at: str):
         self.id = id
         self.name = name
         self.brewery_type = brewery_type
@@ -25,10 +31,34 @@ class Brawery:
         self.created_at = created_at
 
     def __str__(self) -> str:
-        return f'id: {self.id}, \n name: {self.name}, \n brewery_type: {self.brewery_type}, \n street: {self.street}, \n address_2: {self.address_2}, \n address_3: {self.address_3}, \n city: {self.city}, \n state: {self.state}, \n county_province: {self.county_province}, \n postal_code: {self.postal_code}, \n country: {self.postal_code}, \n longitude: {self.longitude}, \n latitude: {self.longitude}, \n phone: {self.phone}, \n website_url: {self.website_url}, \n updated_at: {self.updated_at}, \n created_at: {self.created_at} \n'
+        return f'id: {self.id}, \n name: {self.name}, ' \
+               f'\n brewery_type: {self.brewery_type}, ' \
+               f'\n street: {self.street}, ' \
+               f'\n address_2: {self.address_2}, ' \
+               f'\n address_3: {self.address_3}, ' \
+               f'\n city: {self.city}, ' \
+               f'\n state: {self.state}, ' \
+               f'\n county_province: {self.county_province}, ' \
+               f'\n postal_code: {self.postal_code}, ' \
+               f'\n country: {self.postal_code}, ' \
+               f'\n longitude: {self.longitude}, ' \
+               f'\n latitude: {self.longitude}, ' \
+               f'\n phone: {self.phone}, ' \
+               f'\n website_url: {self.website_url}, ' \
+               f'\n updated_at: {self.updated_at}, ' \
+               f'\n created_at: {self.created_at} \n'
 
 
-lista_20 = [Brawery(dict['id'], dict['name'], dict['brewery_type'], dict['street'], dict['address_2'], dict['address_3'], dict['city'], dict['state'], dict['county_province'], dict['postal_code'], dict['country'], dict['longitude'], dict['latitude'], dict['phone'], dict['website_url'], dict['updated_at'], dict['created_at']) for dict in response[0:20]]
+if __name__ == "__main__":
+    lista_20 = [Brawery(dict['id'], dict['name'],
+                        dict['brewery_type'], dict['street'],
+                        dict['address_2'], dict['address_3'],
+                        dict['city'], dict['state'],
+                        dict['county_province'], dict['postal_code'],
+                        dict['country'], dict['longitude'],
+                        dict['latitude'], dict['phone'], dict['website_url'],
+                        dict['updated_at'], dict['created_at'])
+                for dict in response[0:20]]
 
-for i in lista_20:
-    print(i)
+    for i in lista_20:
+        print(i)

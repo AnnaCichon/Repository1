b4da30272bf7dba9ecbbafe36fd39823cdd7621c
AnnaCichon <anna.cichon97@wp.pl>
2021-12-15 12:53:18 +0100

    dodanie zadań z lab9

---
+++ lab_9/zad3.py
@@ -0,0 +1,7 @@
+from zad1 import czytanie_tekstu
+
+print(czytanie_tekstu('fot1.jpg'))
+print(czytanie_tekstu('fot2.jpg'))
+print(czytanie_tekstu('fot3.jpg'))
+print(czytanie_tekstu('fot4.png'))
+print(czytanie_tekstu('fot5.jpg'))
